106b58bab4261ba5ada67779e33655777e9ac467
Sharadha Iyer <iyer.s.sharadha@gmail.com>
2020-09-09 10:54:49 +0530

    Update header.h

--- header.h
+++ header.h
@@ -1,3 +1,6 @@
+#ifndef HEADER_H
+#define HEADER_H
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <pwd.h>
@@ -19,10 +22,6 @@
 #define DELIM " \t\n\a\r"
 #define ll long long int
 
-
-#define lp(var,start,end) for (ll var = start; var <end ; ++var)
-#define rlp(var,start,end) for(ll var = start; var>=end ; var--)
-
 struct dirent *dir;
 struct stat st;
 ll status[1000005];
@@ -31,29 +30,39 @@ struct group *grp;
 ll LS(char **args);
 struct tm *tm;
 char datestring[1050];
+char **bg_arr;
 #define semi ";"
+ll curr_pid;
 char root[1024];
 ll pos;
 void checkHostName(int hostname);
+bool found;
 char ** parseCommand(char *inp);
 char ** parseSemicolon();
-
+char **bg_arr;
 ll execute(char ** args);
 ll checkCommand(char ** args);
 void ctrlC(int signum);
 void mainloop(void);
 void printPrompt() ;
-char ** breakem(char * a);
 ll CD(char **args);
-char *actual_address2(char ** args,ll st);
+char *real_address(char ** args,ll st);
 char* returnPath (char* cwd);
 ll ECHO(char **args);
 ll PWD(char **args);
-
+ll BG(char **args);
+int gbnum;
 ll PINFO(char **args) ;
 ssize_t bufsize;
-
+char hist[20][1000];
+int hist_i;
 void ctrlZ(int signum);
+void ctrlZ2();
 ll quit(char ** args);
+
 void memory_error();
 int main(int a,char **b);
+
+
+
+#endif
