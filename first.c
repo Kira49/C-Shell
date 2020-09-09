151d7a82db460b8ca31b66f6eb6dc1bba36780f1
Sharadha Iyer <iyer.s.sharadha@gmail.com>
2020-09-09 10:59:34 +0530

    Update first.c

--- first.c
+++ first.c
@@ -1,17 +1,18 @@
 #include"header.h"
 
-
 int main(int arg1, char **arg2)
 {
     getcwd(root, 1024);
     ll state;
+		bg_arr=(char**)malloc(123456*sizeof(char *));
     while(1)
     {
         printPrompt();
-
         char **commands;
         char **argsi;
-         signal(SIGTSTP,ctrlZ);
+				int st;
+				pid_t p;
+        signal(SIGTSTP,ctrlZ);
         signal(SIGINT,ctrlC);
         bufsize = 0;
         commands=parseSemicolon();
@@ -21,6 +22,20 @@ int main(int arg1, char **arg2)
         	{
         		break;
         	}
+        	p = waitpid(-1,&st,WNOHANG);
+					if(p>0)
+					{
+						if(WIFEXITED(st)>0)
+						{
+							status[p]=0;
+							fprintf(stderr, "Process %s with pid: %d exited normally\n",bg_arr[p],p);
+							bg_arr[p]=NULL;
+						}
+						else if(WIFSIGNALED(st))
+						{
+							fprintf(stderr, "Process %s with pid: %d exited abnormally\n",bg_arr[p],p);
+						}
+					}
           bufsize = 100005;
           argsi = parseCommand(commands[i]);
           state=checkCommand(argsi);
