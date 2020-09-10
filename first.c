83ed467ba3c63cfc92442a2ac6f29eb7fd06b9b9
Sharadha Iyer <iyer.s.sharadha@gmail.com>
2020-09-10 10:34:40 +0530

    Update first.c

--- first.c
+++ first.c
@@ -1,20 +1,23 @@
 #include"header.h"
-
+// rhis is the very first file that is executed
 int main(int arg1, char **arg2)
 {
+    // we initialise root
     getcwd(root, 1024);
     ll state;
 		bg_arr=(char**)malloc(123456*sizeof(char *));
     while(1)
     {
+	// invoke the funtion to prin t our prompt, wchich is written in invoke.c
         printPrompt();
         char **commands;
         char **argsi;
-				int st;
-				pid_t p;
+	int st;
+	pid_t p;
         signal(SIGTSTP,ctrlZ);
         signal(SIGINT,ctrlC);
         bufsize = 0;
+	// call function to take in and parse the commands separated by a semicolon
         commands=parseSemicolon();
         for (ll i=0; i>=0 ;i++)
         {
@@ -23,19 +26,21 @@ int main(int arg1, char **arg2)
         		break;
         	}
         	p = waitpid(-1,&st,WNOHANG);
-					if(p>0)
-					{
-						if(WIFEXITED(st)>0)
-						{
-							fprintf(stderr, "Process %s with pid-- %d has exited normally\n",bg_arr[p],p);
-							bg_arr[p]=NULL;
-						}
-						else if(WIFSIGNALED(st))
-						{
-							fprintf(stderr, "Process %s with pid-- %d has exited abnormally\n",bg_arr[p],p);
-						}
-					}
+		if(p>0)
+		{
+			// check if any of the previously executed background processes have completed
+			if(WIFEXITED(st)>0)
+			{
+				fprintf(stderr, "Process %s with pid-- %d has exited normally\n",bg_arr[p],p);
+				bg_arr[p]=NULL;
+			}
+			else if(WIFSIGNALED(st))
+			{
+				fprintf(stderr, "Process %s with pid-- %d has exited abnormally\n",bg_arr[p],p);
+			}
+		}
           bufsize = 100005;
+	  // call funtions to parse the commands futher, and then send then off for execution
           argsi = parseCommand(commands[i]);
           state=checkCommand(argsi);
         }
