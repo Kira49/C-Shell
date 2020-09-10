634ea95292f6d5a1c579306a21524409f3f5f41e
Sharadha Iyer <iyer.s.sharadha@gmail.com>
2020-09-10 10:29:44 +0530

    Update execute.c

--- execute.c
+++ execute.c
@@ -1,17 +1,17 @@
 #include"header.h"
-
+// in this file we check the parsed command and redirect it to be executed by the appropriate function
 ll checkCommand(char ** args)
 {
-	ll cnt=0, notfound=0;
-
+	notfound=0;
+	// if no command is found
 	if(args[0]==NULL)
 	{
 		return EXIT_FAILURE;
 	}
 	if(strcmp(args[0],"quit")==0)
-  {
-    	return quit(args);
-  }
+        {
+    		return quit(args);
+        }
 	if(strcmp(args[0],"cd")==0)
 	{
 		return CD(args);
@@ -26,14 +26,15 @@ ll checkCommand(char ** args)
 	}
 	if(strcmp(args[0],"ls")==0)
 	{
-        return LS(args);
+       		return LS(args);
 	}
-  if(strcmp(args[0],"pinfo")==0)
-  {
-      return PINFO(args);
-  }
-  if(notfound==0)
-  {
+        if(strcmp(args[0],"pinfo")==0)
+  	{
+        	return PINFO(args);
+  	}
+	// if the command isnt any of the inbuilt ones, we send it to be checked for background process and be executed
+  	if(notfound==0)
+  	{
 		return BG(args);
-  }
+  	}
 }
