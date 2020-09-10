e4a90337cc0c719400ae84150fbab725626aa554
Sharadha Iyer <iyer.s.sharadha@gmail.com>
2020-09-10 10:26:36 +0530

    Update errorHandling.c

--- errorHandling.c
+++ errorHandling.c
@@ -1,4 +1,6 @@
 #include"header.h"
+// this file contains a few utility functions to handle errors and interrupts
+// handles error while getting name of host
 void checkHostName(int hostname)
 {
     if (hostname == -1)
@@ -7,22 +9,26 @@ void checkHostName(int hostname)
         exit(1);
     }
 }
+// helps you quit the terminal
 ll quit(char ** args)
 {
     exit(EXIT_SUCCESS);
     return 0;
 }
+// handles errors in memory allocation
 void memory_error()
 {
 	fprintf(stderr, "malloc error\n" );
         exit(EXIT_FAILURE);
 }
+// handles ctrl C keyboard interrupt
 void ctrlC(int signum)
 {
     fprintf(stdout,"\nKeyboard interupt ^C, press enter to continue" );
     fflush(stdout);
 		return;
 }
+// handle ctrl Z
 void ctrlZ(int signum)
 {
 	return 	;
