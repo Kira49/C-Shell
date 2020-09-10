0a90f66e7ca2a5782cf524e82cf27053de8fadfe
Sharadha Iyer <iyer.s.sharadha@gmail.com>
2020-09-10 10:17:44 +0530

    Update bg.c

--- bg.c
+++ bg.c
@@ -1,9 +1,13 @@
 #include "header.h"
+// in this file, we wheck the user input to see if a background process is expected, and try to execute the background and foreground processes.
+// if bg not expected, we execute the command normallyboth bg and fg despite being named as bg for convenience 
+// this file contains 
 ll BG(char **args)
 {
     bool bg=false;
     int st;
     ll i=0;
+    // checkinf if bg expected	
     while(args[i]!=NULL)
     {
         if(strcmp(args[i],"&")==0)
@@ -17,10 +21,10 @@ ll BG(char **args)
         }
         i++;
     }
-
+    //forking to create a child process. hereon, these lines of code are executed twice
     pid_t pid=fork();
 
-
+    // error handling
     if (pid < 0)
     {
         perror("ERROR:");
@@ -28,10 +32,12 @@ ll BG(char **args)
     }
     else if(pid==0)
     {
+	// vi command is an execption to our background processes since it causes issues. running vi & on terminal exits process
     	if(bg==true&&strcmp(args[0],"vi")==0)
 			{
 				exit(EXIT_SUCCESS);
 			}
+	// checking and executing commands
       if ( execvp(args[0], args) < 0)
       {
           fprintf(stderr,"command: %s not found, press Enter to continue\n",args[0] );
@@ -39,7 +45,7 @@ ll BG(char **args)
       }
     	return 0;
     }
-
+    //waiting for process to complete if it isnt mentioned as background
     if(bg==false)
     {
 			bg_arr[pid]=args[0];
