d8b6fa79783b813b5863be20e364ce746aa54a01
Sharadha Iyer <iyer.s.sharadha@gmail.com>
2020-09-10 10:24:21 +0530

    Update echo.c

--- echo.c
+++ echo.c
@@ -1,13 +1,15 @@
 #include"header.h"
+// in this file we handle echho command.
+// not yet taken quotes and environment variables into consideration
 ll ECHO(char **args)
 {
-
+    // if no input is given after echo
     if(args[1]==NULL)
     {
     	printf("\n");
     	return 1;
     }
-
+    // printing all arguments given back to the terminal
     ll i=1;
     while(args[i]!=NULL)
     {
