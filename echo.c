b18bc4c1345108189974f539e8166cc20623f3f9
Sharadha Iyer <iyer.s.sharadha@gmail.com>
2020-09-29 11:57:51 +0530

    Update echo.c

--- echo.c
+++ echo.c
@@ -9,6 +9,24 @@ ll ECHO(char **args)
     	printf("\n");
     	return 1;
     }
+    char a[1000];
+    ll x= strlen(args[1]);
+    // printf("%c\n",args[1][1] );
+    if(args[1][0]=='$')
+    {
+        for(ll i=1; i<x; i++)
+        {
+            a[i-1]=args[1][i];
+        }
+        a[x]='\0';
+        if(getenv(a)==NULL)
+        {
+            perror("not a env:");
+            return 0;
+        }
+        printf("%s\n",getenv(a));
+        return 1;
+    }
     // printing all arguments given back to the terminal
     ll i=1;
     while(args[i]!=NULL)
@@ -16,6 +34,6 @@ ll ECHO(char **args)
         printf("%s ",args[i] );
         i++;
     }
-    printf("\n");    	
+    printf("\n");
     return 1;
 }
