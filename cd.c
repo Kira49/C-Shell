e06c394f27a7f0f018a6b8b54ea1b8be8ca84db0
Sharadha Iyer <iyer.s.sharadha@gmail.com>
2020-10-02 19:56:42 +0530

    Update cd.c

--- cd.c
+++ cd.c
@@ -7,19 +7,42 @@ ll CD(char **args)
     if (args[1]==NULL)
     {
         chdir(root);
-        return 1;        
-    } 
-    
+        for(int i=0; i<1024; i++)
+        {
+            prev[i] = root[i];
+        }
+        return 1;
+    }
+    if(strcmp(args[1], "-")!=0)
+    {
+        getcwd(prev, 1024);
+    }
     // when input contains more than 1 argument (cd expects max 1)
     if (args[2]!=NULL)
     {
         perror("Couldn't run cd command");
-        return 1;        
+        return 1;
+    }
+    if(strcmp(args[1], "-")==0)
+    {
+        char temp[1024];
+        getcwd(temp, 1024);
+        printf("%s\n", prev);
+        chdir(prev);
+        for(int i=0; i<1024; i++)
+        {
+            prev[i] = temp[i];
+        }
+        return 1;
     }
     // if path given as root
     if (strcmp(args[1], "~") == 0)
     {
         chdir(root);
+        for(int i=0; i<1024; i++)
+        {
+            prev[i] = root[i];
+        }
         return 1;
     }
     // if path given as current directory
@@ -28,10 +51,10 @@ ll CD(char **args)
         return 1;
     }
     // if address relarive to home is given
-    if (args[1][0] == '~') 
+    if (args[1][0] == '~')
     {
         char *path=(char *) malloc(1234*sizeof(char));
-        
+
         sprintf(path, "%s/%s", root, args[1] + 2);
         if (chdir(path) < 0)
             perror("Path error");
