cb53789092df94283e6bf17a7a92da8b2705734c
Sharadha Iyer <iyer.s.sharadha@gmail.com>
2020-10-02 20:06:07 +0530

    Update piping.c

--- piping.c
+++ piping.c
@@ -2,78 +2,68 @@
 void piping(char **args)
 {
 	ll i=0,pipes=0;
-	//counting number of pipes
-	while(args[i]!=NULL)
-	{
-		if(strcmp(args[i],"|")==0)
-		{
-			pipes++;
-		}
-		i++;
-	}
-	ll sz=i;
-	i=0;
-	ll j=0,finish=0;
-	ll cnt=0;
-	char *cur_com[1000];
-	ll frd;
 	while(1)
 	{
-		if(args[i]==NULL)break;
-		if(finish==1)break;
+		if(args[i]==NULL)
+		{
+			break;
+		}
+		if(strcmp(args[i],"|")!=0)
+		{
+			i++;
+			continue;
+		}
+		pipes++;
+		i++;
+	}
+	ll j=0,finish=0, cnt=0, sz=i,frd;
+	i=0;
+	char *cur_com[1000];
+	while(args[i]!=NULL && finish!=1)
+	{
 		ll p=0;
-		while(1)
+		while(finish!=1)
 		{
 			if(args[j]==NULL)
 			{
 				finish=1;
+				continue;
+			}
+			if(strcmp(args[j],"|")==0)
+			{
 				break;
 			}
-			if(strcmp(args[j],"|")!=0)
+			else
 			{
 				cur_com[p]=args[j];
-				// printf("%s\n",cur_com[p]);
 				p++;
-			}
-			else break;
-			if(args[j]==NULL)
-			{
-				finish=1;
-				break;
 			}
 			j++;
 		}
-		cnt++;
-		j++;
 		cur_com[p]=NULL;
-		ll z=0;
-		int fd[2];
-		int in,out;
-		// printf("%lld\n",finish );
+		cnt++, j++;
+		int fd[2], in,out;
 		if(cnt==1)
 		{
-			if(pipe(fd)==-1)
+			if(pipe(fd)<0)
 			{
-				perror("piping error :");
+				perror("Piping error:");
 				return;
 			}
 			ll z=0;
 			char infile[1000];
-			while(cur_com[z]!=NULL)
+			while(1)
 			{
-				// printf("%s\n",cur_com[z] );
+				if(cur_com[z]==NULL)
+				{
+					break;
+				}
 				if(strcmp(cur_com[z],"<")==0)
 				{
-					cur_com[z]=NULL;
-					strcpy(infile,cur_com[z+1]);
-					// ll zz=0;
-					// while(cur_com[zz]!=NULL)
-					// {
-						// printf("%s\n",cur_com[zz] );
-						// zz++;
-					// }
 					in=dup(0);
+					strcpy(infile,cur_com[z+1]);
 			   		frd = open(infile,O_RDONLY, 0644);
+					cur_com[z]=NULL;
 					dup2(frd,0);
 				}
 				z++;
@@ -82,34 +72,24 @@ void piping(char **args)
 			dup2(fd[1],1);
 			close(fd[1]);
 		}
-		else if(finish==1)
+		if(finish==1 && cnt!=1)
 		{
-			// redirect
-			ll z=0;
 			char outfile[1000];
-			ll flagg=0;
-			while(cur_com[z]!=NULL)
+			ll z=0;
+			while(1)
 			{
-				// printf("%s\n",cur_com[z] );
-				if(strcmp(cur_com[z],">")==0)
+				if(cur_com[z]==NULL)
 				{
-					cur_com[z]=NULL;
-					strcpy(outfile,cur_com[z+1]);
-					out=dup(1);
-					frd= open(outfile, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-					dup2(frd,1);
-					close(frd);
-					flagg=1;
+					break;
 				}
-				else if(strcmp(cur_com[z],">>")==0)
+				if(strcmp(cur_com[z],">")==0 || strcmp(cur_com[z],">>")==0)
 				{
-					cur_com[z]=NULL;
-					strcpy(outfile,cur_com[z+1]);
 					out=dup(1);
-					frd = open(outfile, O_WRONLY | O_CREAT | O_APPEND, 0644);
+					strcpy(outfile,cur_com[z+1]);
+					frd= open(outfile, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+					cur_com[z]=NULL;
 					dup2(frd,1);
 					close(frd);
-					flagg=1;
 				}
 				z++;
 			}
@@ -117,36 +97,34 @@ void piping(char **args)
 			dup2(fd[0],0);
 			close(fd[0]);
 		}
-		else
+		if(finish!=1 && cnt!=1)
 		{
-			in = dup(0);
-			dup2(fd[0],0);
-			if(pipe(fd)==-1)
+			in = dup(0), dup2(fd[0],0);
+			if(pipe(fd)<0)
 			{
-				perror("piping error :");
+				perror("Piping error :");
 				return;
 			}
-			out=dup(1);
-			dup2(fd[1],1);
+			out=dup(1), dup2(fd[1],1);
 			close(fd[1]);
 		}
 		pid_t pid;
 		pid=fork();
 
-		if(pid==-1)
+		if(pid<0)
 		{
-			perror("fork error : ");
+			perror("Fork error : ");
 			return;
 		}
-		else if(!pid)
+		if(pid==0)
 		{
-			if(execvp(cur_com[0],cur_com)==-1)
+			if(execvp(cur_com[0],cur_com)<0)
 			{
-				perror("command not found");
+				perror("Command not found");
 				exit(EXIT_FAILURE);
 			}
 		}
-		else
+		if(pid>0)
 		{
 			wait(NULL);
 			dup2(in,0);
